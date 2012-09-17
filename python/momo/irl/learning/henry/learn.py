Fix forward backward and gradient computation

python/momo/irl/learning/henry/learn.py
@@ -1,5 +1,7 @@
 import numpy as np
+import pylab as pl
 import momo
+from math import *
 
 def learn( feature_module, convert, frame_data, ids, radius, replan ):
   feature_length = feature_module.FEATURE_LENGTH
@@ -10,16 +12,27 @@ def learn( feature_module, convert, frame_data, ids, radius, replan ):
 
   # Initialize weight vector
   w  = np.random.rand( feature_length )
-  w /= np.linalg.norm( w )
 
+  gamma = 1.0
   for o_id in ids:
-    gradient = compute_gradient( 
-      feature_module, planner, convert, compute_features,
-      frame_data[o_id]["states"], frame_data[o_id]["frames"],
-      w, replan
-    )
-    for i in xrange( feature_length ):
-      w[i] *= exp( -gamma * gradient[i] )
+    w /= np.linalg.norm( w )
+    for i in xrange( 100 ):
+      gradient = compute_gradient( 
+        feature_module, planner, convert, compute_features,
+        frame_data[o_id]["states"], frame_data[o_id]["frames"],
+        w, replan
+      )
+      if np.any( np.isnan( gradient ) ):
+        print "w, nan", w
+        break
+      for i in xrange( feature_length ):
+        w[i] *= exp( -gamma * gradient[i] )
+      gamma *= 0.999
+      magnitude = np.linalg.norm( gradient )
+      print "gradient", magnitude
+      print gamma
+      if magnitude < 0.05:
+        break
 
   return w
 
@@ -29,35 +42,40 @@ def compute_gradient( feature_module, planner, convert, compute_features, states
     [convert.to_world2( convert.from_world2( s ), np.linalg.norm( s[2:] ) ) for s in states], 
     frames 
   )
+  mu_observed /= np.sum( mu_observed[:4] )
 
   velocities = [np.linalg.norm( v[2:] ) for v in states]
   avg_velocity = np.sum( velocities, 0 ) / len( velocities )
 
-  forward, backward, costs = planner( states[0], states[-1], avg_velocity, frames[0], w )
+  pl.figure( 0 )
+  pl.ion()
+  forward, backward, costs = planner( states[0], states[-1], avg_velocity, frames[0], w  * 2 )
   features = compute_features( avg_velocity, frames[0] )
 
   mu_expected = np.zeros( feature_module.FEATURE_LENGTH )
-  
+  tmp = forward * 0.
+
   for d in xrange( forward.shape[0] ):
     for y in xrange( forward.shape[1] ):
       for x in xrange( forward.shape[2] ):
-        for d1 in xrange( d - 1, d + 2 ):
-          if d1 > 7:
-            d1 -= 4
-          elif d1 < 0:
-            d1 += 4
+        #tmp[d, y, x] = forward[d, y, x] * exp( -np.dot( w, features[d, y, x] ) ) * backward[d, y, x] 
+        tmp[d, y, x] = forward[d, y, x] * exp( -costs[d, y, x] ) * backward[d, y, x] 
+  for d in xrange( forward.shape[0] ):
+    for y in xrange( forward.shape[1] ):
+      for x in xrange( forward.shape[2] ):
+        mu_expected += features[d, y, x] * tmp[d, y, x]
+  mu_expected /= np.sum( mu_expected[:4] )
 
-          x1 = x + feature_module.DIRECTIONS[d1][0]
-          y1 = y + feature_module.DIRECTIONS[d1][1]
-          
-          #if x1 >= 0 and x1 < forward.shape[2] and y1 >= 0 and y1 < forward.shape[1]:
-            #mu_expected += features[d1, y1, x1] * forward[d, y, x] * costs[d1, y1, x1] * backward[d1, y1, x1]  
+  pl.figure( 1 )
+  pl.ion()
+  pl.clf()
+  pl.imshow( np.sum( tmp, 0 ) )
+  pl.draw()
+  np.set_printoptions( precision = 3 )
+  np.set_printoptions( suppress = True )
+  print "mu_observed", mu_observed
+  print "mu_expected", mu_expected
 
-  start = convert.from_world2( states[0] )
-  goal  = convert.from_world2( states[-1] )
-
-  print "forward", forward[tuple( reversed( goal.tolist() ) )]
-  print "backward", backward[tuple( reversed( start.tolist() ) )]
 
   return mu_observed - mu_expected
 

python/momo/planning/forward_backward.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+BASE_DIR = os.path.abspath( os.path.join( os.path.dirname( __file__ ), "../../.." ) )
+path     = os.path.abspath( os.path.join( BASE_DIR, "python" ) )
+sys.path.append( path )
+
 import pyopencl as cl
 import numpy as np
 import pylab as pl
@@ -29,33 +36,56 @@ class forward_backward( momo.opencl.Program ):
     ints[tuple( reversed( start.tolist() ) )] = 1
 
     cost_buffer  = cl.Buffer( self.context, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf = costs.astype( np.float32 ) )
-    m1_buffer  = cl.Buffer( self.context, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf = ints )
-    m2_buffer  = cl.Buffer( self.context, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf = ints )
+    mask_buffer = cl.Buffer( self.context, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf = ints )
     f1_buffer  = cl.Buffer( self.context, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf = floats )
     f2_buffer  = cl.Buffer( self.context, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf = floats )
     
-    pl.ion()
 
-    for i in xrange( 300 ):
-      e1 = self.flow.computeForward1( 
-        self.queue, costs.shape, None, 
-        np.int32( width ), np.int32( height ), np.int32( sign ),
-        self.idirection_buffer, cost_buffer,
-        m1_buffer, m2_buffer, 
-        f1_buffer, f2_buffer
-      )
-      e2 = self.flow.computeForward2( 
+    for i in xrange( sum( costs.shape ) ):
+      if sign > 0:
+        e1 = self.flow.forwardPass( 
+          self.queue, costs.shape, None, 
+          np.int32( width ), np.int32( height ),
+          self.idirection_buffer, cost_buffer,
+          mask_buffer,  
+          f1_buffer, f2_buffer
+        )
+      else:
+        e1 = self.flow.backwardPass( 
+          self.queue, costs.shape, None, 
+          np.int32( width ), np.int32( height ),
+          self.idirection_buffer, cost_buffer,
+          mask_buffer,  
+          f1_buffer, f2_buffer
+        )
+      e2 = self.flow.updatePass( 
         self.queue, costs.shape, None, 
         np.int32( width ), np.int32( height ),
-        m1_buffer, m2_buffer, 
         f1_buffer, f2_buffer,
         wait_for = [e1] 
       )
       e2.wait()
-      cl.enqueue_copy( self.queue, floats, f1_buffer )
-      print i
-      pl.clf()
-      pl.imshow( floats[4] )
-      pl.draw()
     cl.enqueue_copy( self.queue, floats, f1_buffer )
+    pl.ion()
+    pl.clf()
+    pl.imshow( np.sum( floats, 0 ) )
+    pl.draw()
     return floats
+
+
+if __name__ == "__main__":
+  costs = np.ones( (8, 7, 8 ) )
+  for i in xrange( 4 ):
+    costs[i] *= ( i + 1 ) * 0.1
+    if i != 0:
+      costs[8 - i] *= ( i + 1 ) * 0.1
+
+  start = np.array( [1, 3, 0], dtype = np.int32 )
+  end = np.array( [6, 3, 0], dtype = np.int32 )
+
+  fb = forward_backward()
+
+  f = fb( costs, start, 1 )
+  b = fb( costs, end, -1 )
+  print b[0, 3, 1], f[6, 3, 0]
+